aaba48b279cae733de49902c9c1b6266ff72ea3c
Alessandra Montanher <alessandraamontanher@gmail.com>
2020-01-27 21:11:11 -0300

    inserting in datatable

--- week-3/palindromo-challenge/index.js
+++ week-3/palindromo-challenge/index.js
@@ -1,8 +1,10 @@
 const inputPalindromo = document.querySelector("#palindromoValue");
 const btnDeleteAll = document.querySelector("#delete-all");
+const tablePalindromos = document.querySelector('#table-palindromos');
 const KEY_ENTER = 13;
 
 inputPalindromo.addEventListener("keypress", handleSubmitForm);
+btnDeleteAll.addEventListener('click', handleDeleteAll);
 
 function handleSubmitForm(event){
     const isEnter = event.keyCode === KEY_ENTER;
@@ -22,13 +24,35 @@ function handleSubmitForm(event){
     }
 }
 
+function handleDeleteAll() {
+    tablePalindromos.lastElementChild.innerHTML = '';
+}
+
 function checkPalindrome(value){
     const valueInvertido = value.split('').reverse().join('');
     return value === valueInvertido;
 }
 
 function createDataTable(object){
-console.log(object);
+    const tableRow = createTableRow();
+    const tableDataPalindromo = createTableData(object.valor);
+    const tableDataValor = createTableData(object.isPalindrome);
+
+    tableRow.appendChild(tableDataPalindromo);
+    tableRow.appendChild(tableDataValor);
+
+    tablePalindromos.children[1].appendChild(tableRow);
+}
+
+function createTableRow() {
+    const row = document.createElement('tr');
+    return row;
+}
+
+function createTableData(valor){
+    const data = document.createElement('td');
+    data.textContent = valor;
+    return data;
 }
 
 function cleanInput(){
