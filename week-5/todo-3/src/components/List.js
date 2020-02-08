95003c746632be59704e4d4a35d39d73c470065b
Alessandra <alessandraamontanher@gmail.com>
2020-02-08 17:28:52 -0300

    add style green

--- week-5/todo-3/src/components/List.js
+++ week-5/todo-3/src/components/List.js
@@ -5,11 +5,9 @@ export default function List({ todos, onClick }) {
     <ul>
       {
         todos.map((todo, index) => {
-          if (true) { };
           return <li key={todo.id}
             onClick={() => onClick(index)}
             style={{backgroundColor: todo.done ? "green" : ""}}>
-            {todo.done && "✓ "}
             {todo.name}
           </li>
         })
