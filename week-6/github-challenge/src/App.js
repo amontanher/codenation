751c136bd3ae0191369c8766335adf297beca5be
alessandra <alessandraamontanher@gmail.com>
2020-02-21 10:30:22 -0300

    regex validation

--- week-6/github-challenge/src/App.js
+++ week-6/github-challenge/src/App.js
@@ -3,25 +3,40 @@ import Input from './components/Input';
 import { getRepositoriesByUser } from './services/api';
 
 function App() {
-  const [entrada, setEntrada] = React.useState("");
+  const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+  const [entrada, setEntrada] = React.useState('');
+  const [error, setError] = React.useState(false);
 
   const handleEntrada = ({ target }) => {
+    debugger;
     const { value } = target;
 
+    if (!regex.test(value)) {
+      setError(true);
+    } else {
+      setError(false);
+    }
     setEntrada(value);
-  }
+  };
 
   const handleKeyPress = ({ key }) => {
     if (key === 'Enter') {
       getRepositoriesByUser(entrada).then(json => console.log(json));
     }
-  }
+  };
 
   return (
     <div>
       <h3>GitHub</h3>
       <p>Veja os repositórios do seu usuário favorito!</p>
-      <Input value={entrada} onChange={handleEntrada} onKeyPress={handleKeyPress} />
+      <Input
+        value={entrada}
+        onChange={handleEntrada}
+        onKeyPress={handleKeyPress}
+        hasError={error}
+      />
+      {error && <p>User inválido</p>}
     </div>
   );
 }
--- week-6/github-challenge/src/components/Input.js
+++ week-6/github-challenge/src/components/Input.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Input({ value, onChange, onKeyPress }) {
-    return (<input value={value} onChange={onChange} onKeyPress={onKeyPress} placeholder="Insira o nome do usuário"></input>)
+export default function Input({ value, onChange, onKeyPress, hasError }) {
+  const style = {};
+
+  if (hasError) {
+    style.border = '3px solid red';
+  }
+
+  return (
+    <input
+      value={value}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+      style={style}
+      placeholder="Insira o nome do usuário"
+    ></input>
+  );
 }
