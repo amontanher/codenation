4a0a2195fe9fbfb852134400afe37be4f023e78e
alessandra <alessandraamontanher@gmail.com>
2020-02-26 15:52:50 -0300

    style

--- week-6/marvel-api/src/App.js
+++ week-6/marvel-api/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import Header from './components/Header'
-import Heroes from './components/Heroes'
-import { getCharacters, getCharactersByName } from './services/api'
+import Header from './components/Header';
+import Heroes from './components/Heroes';
+import { getCharacters, getCharactersByName } from './services/api';
 
 function App() {
-  const [entrada, setEntrada] = React.useState("");
+  const [entrada, setEntrada] = React.useState('');
   const [erro, setErro] = React.useState(null);
   const [heroes, setHeroes] = React.useState([]);
 
   const handleChangeInput = ({ target }) => {
     const { value } = target;
 
-    if (value.includes("@")) {
-      setErro("Caracter Inválido");
+    if (value.includes('@')) {
+      setErro('Caracter Inválido');
     } else {
-      setErro("");
+      setErro('');
     }
 
     setEntrada(value);
-  }
+  };
 
   /**
    *Toda vez que usar async await usar try/catch
@@ -26,25 +26,30 @@ function App() {
    */
   const handleKeyPress = event => {
     if (event.key === 'Enter') {
-      getCharactersByName(entrada)
-        .then(json => setHeroes(json.data.results))
-        .catch(e => {
-          //fazer a logica do 404 aqui
-          setHeroes([]);
-        });
+      // getCharactersByName(entrada)
+      //   .then(json => setHeroes(json.data.results))
+      //   .catch(e => {
+      //     //fazer a logica do 404 aqui
+      //     setHeroes([]);
+      //   });
     }
-  }
+  };
 
   React.useEffect(() => {
-    getCharacters().then(json => setHeroes(json.data.results));
-  }, [])
+    // getCharacters().then(json => setHeroes(json.data.results));
+  }, []);
 
   return (
     <div>
-      <Header value={entrada} onChange={handleChangeInput} erro={erro} onKeyPress={handleKeyPress} />
+      <Header
+        value={entrada}
+        onChange={handleChangeInput}
+        erro={erro}
+        onKeyPress={handleKeyPress}
+      />
       {heroes.length ? <Heroes heroes={heroes} /> : <p>404 - não encontrado</p>}
     </div>
-  )
+  );
 }
 
 export default App;
--- week-6/marvel-api/src/components/Header.js
+++ week-6/marvel-api/src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Input from './Input';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Hero, Container, Heading } from 'react-bulma-components';
 
-export default function Header({ value, onChange, erro, onKeyPress}) {
-    return (
-        <div>
-            <Input value={value} onChange={onChange} erro={erro} onKeyPress={onKeyPress}/>
-        </div>
-    )
+export default function Header({ value, onChange, erro, onKeyPress }) {
+  return (
+    <div>
+      <Hero color="danger">
+        <Hero.Body>
+          <Container>
+            <Heading>Hero Search</Heading>
+            <Input
+              value={value}
+              onChange={onChange}
+              erro={erro}
+              onKeyPress={onKeyPress}
+            />
+          </Container>
+        </Hero.Body>
+      </Hero>
+    </div>
+  );
 }
