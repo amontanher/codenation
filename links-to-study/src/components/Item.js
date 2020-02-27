e5cd9fee3ad1826135499bc80ffef3f52f3b29c4
alessandra <alessandraamontanher@gmail.com>
2020-02-27 14:39:16 -0300

    item style

--- links-to-study/src/components/Item.js
+++ links-to-study/src/components/Item.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './../style/ItemStyle.css';
 
 export default function Item({ link }) {
+  const handleClick = url => {
+    window.open(url, '_blank');
+  };
+
   return (
     <div id="card">
       <div>{link.title}</div>
-      <div>{link.url}</div>
+      <div>
+        <button id="link" onClick={() => handleClick(link.url)}>
+          ACESSAR
+        </button>
+      </div>
     </div>
   );
 }
