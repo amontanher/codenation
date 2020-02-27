ee5b84cdc8861093c691b649d23df59266ba684f
alessandra <alessandraamontanher@gmail.com>
2020-02-27 15:09:54 -0300

    combo box render

--- links-to-study/src/App.js
+++ links-to-study/src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
 import data from './data/links.json';
+import Header from './components/Header';
 import List from './components/List';
 
 function App() {
+  const [links, setLinks] = React.useState([{}]);
+  const [modulo, setModulo] = React.useState(0);
+
+  React.useEffect(() => {
+    setLinks(data.contents[0].links);
+  }, []);
+
+  const handleChange = e => {
+    setModulo(e.target.value);
+    setLinks(data.contents[modulo].links);
+  };
+
   return (
     <div id="main">
       <header id="header">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
+        <Header
+          size={data.contents.length}
+          modulo={modulo}
+          handleChange={handleChange}
+        />
       </header>
       <br />
-      <List links={data.contents[0].links} />
+      <List links={links} />
     </div>
   );
 }
---
+++ links-to-study/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './../style/HeaderStyle.css';
+
+export default function Header({ size, module, handleChange }) {
+  return (
+    <div id="head">
+      <div id="title">
+        <h3>React</h3>
+        <h4>Jornada de desafios de React</h4>
+      </div>
+      <div>
+        <select value={module} onChange={handleChange}>
+          <option value="0">Module 1</option>
+          <option value="1">Module 2</option>
+          <option value="2">Module 3</option>
+          <option value="3">Module 4</option>
+          <option value="4">Module 5</option>
+          <option value="5">Module 6</option>
+        </select>
+      </div>
+    </div>
+  );
+}
