406f777aa13a3ca3488e954c5569acc5bb568e85
Alessandra <alessandraamontanher@gmail.com>
2020-03-11 21:43:02 -0300

    home component

--- week-8/agenda-telefonica-redux/src/routes/index.js
+++ week-8/agenda-telefonica-redux/src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import NotFound from '../pages/NotFound/NotFound'
+import NotFound from '../pages/NotFound/NotFound';
+import Home from '../pages/Home/Home';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" render={() => <div>HOME</div>}></Route>
+      <Route exact path="/" component={Home}></Route>
       <Route exact path="/create" render={() => <div>CREATE</div>}></Route>
       <Route
         exact
