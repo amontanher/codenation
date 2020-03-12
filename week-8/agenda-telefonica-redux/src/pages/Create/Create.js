485ac6e95a915f9bc7ff2ecfe8b571cf5011a060
Alessandra <alessandraamontanher@gmail.com>
2020-03-11 22:21:04 -0300

    usestate create

--- week-8/agenda-telefonica-redux/src/pages/Create/Create.js
+++ week-8/agenda-telefonica-redux/src/pages/Create/Create.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Create() {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+
     return (
         <div>
             <label>Nome</label>
-            <input type="text" />
+            <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}/>
 
             <label>E-mail</label>
-            <input type="email" />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+
+            <button>Criar Contato</button>
         </div>
     );
 }
