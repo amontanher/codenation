1d463d7b54438e6a987660641904a650b0d58f58
Alessandra <alessandraamontanher@gmail.com>
2020-04-08 20:43:14 -0300

    styling

--- week-8/agenda-telefonica-redux/src/pages/Create/Create.js
+++ week-8/agenda-telefonica-redux/src/pages/Create/Create.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addContato } from '../../store/actions/contatoActions';
 import { Link } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
-
-import Button from '../../components/Button'
+import { FaArrowLeft, FaSave } from 'react-icons/fa';
 
 export default function Create() {
   const [nome, setNome] = useState('');
@@ -12,9 +10,22 @@ export default function Create() {
   const dispatch = useDispatch();
 
   const createContact = () => {
-    const contato = { nome: nome, email: email };
-    console.log(contato);
-    dispatch(addContato(contato));
+    // const contato = { nome: nome, email: email };
+    // console.log(contato);
+    // dispatch(addContato(contato));
+    console.log("teste");
+  };
+
+  const style = {
+    backgroundColor: '#57DB64',
+    color: 'white',
+    border: 0,
+    borderRadius: 5,
+    padding: 5,
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none'
   };
 
   return (
@@ -22,19 +33,19 @@ export default function Create() {
       <div className="header">
         <h1>Novo contato</h1>
         <Link className="back-link" to="/"><FaArrowLeft /> Voltar</Link>
-        <Button title="Criar Contato" icon="FaSave" color="#57DB64" />
+        <Link style={style} to="#" onClick={createContact}><FaSave /> Criar Contato</Link>
       </div>
-      {/* <label>Nome</label>
-      <input type="text" value={nome} onChange={e => setNome(e.target.value)} />
+      <div className="container-inputs">
+        <label>Nome</label>
+        <input type="text" value={nome} onChange={e => setNome(e.target.value)} />
 
-      <label>E-mail</label>
-      <input
-        type="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-
-      <button onClick={createContact}>Criar Contato</button> */}
+        <label>E-mail</label>
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
--- week-8/agenda-telefonica-redux/src/pages/Home/Home.js
+++ week-8/agenda-telefonica-redux/src/pages/Home/Home.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-import Button from '../../components/Button'
+import { FaPlus } from 'react-icons/fa';
 
 import './styles.css';
 
 export default function Home() {
   const { contatos } = useSelector(({ contatos }) => contatos);
 
+  const style = {
+    backgroundColor: '#6FB0FC',
+    color: 'white',
+    border: 0,
+    borderRadius: 5,
+    padding: 5,
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none'
+  };
+
   return (
     <div className="container">
       <div className="header">
         <h1>Contatos</h1>
         <p className="contact-length">({contatos.length})</p>
-        <Button title="Adicionar Contato" icon="FaPlus" color="#6FB0FC" click="/create" />
+        <Link style={style} to="/create"><FaPlus /> Adicionar Contato</Link>
       </div>
       {contatos.map((contato, index) => (
         <div key={index} className="container-contact">
