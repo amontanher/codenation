282174ec56da7a94d81c73db0102ff2ada03bc41
Alessandra <alessandraamontanher@gmail.com>
2020-04-10 17:20:08 -0300

    route 404

---
+++ week-8/pokedex-redux/src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NotFoundImg from '../../assets/notfound.png';
+
+export default function NotFound() {
+    return <div>
+        <img src={NotFoundImg} />
+    </div>;
+}
--- week-8/pokedex-redux/src/routes.js
+++ week-8/pokedex-redux/src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../src/pages/Home';
 import Pokemon from '../src/pages/Pokemon';
+import NotFound from '../src/pages/NotFound';
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/pokemon/:id" component={Pokemon}></Route>
+            <Route exact path="/404" component={NotFound}></Route>
+            <Route exact path="*" component={NotFound}></Route>
         </Switch>
     )
 }
