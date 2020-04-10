24de2f887b6aa41de0fca04b2d2c11d4f38f3068
Alessandra <alessandraamontanher@gmail.com>
2020-04-10 18:31:45 -0300

    notfound style

--- week-8/pokedex-redux/src/pages/NotFound/index.js
+++ week-8/pokedex-redux/src/pages/NotFound/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NotFoundImg from '../../assets/notfound.png';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 export default function NotFound() {
-    return <div>
+    return <div className="nf-container">
         <img src={NotFoundImg} />
+        <h3>Uh-oh!</h3>
+        <p>You look lost on your journey!</p>
+        <Link className="back-link" to="/">Go Back Home</Link>
     </div>;
 }
