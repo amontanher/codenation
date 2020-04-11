bbed490874b5260ddf9e6783d2d9774f6334399e
Alessandra <alessandraamontanher@gmail.com>
2020-04-11 00:28:27 -0300

    pokemon detail page

--- week-8/pokedex-redux/src/pages/Pokemon/index.js
+++ week-8/pokedex-redux/src/pages/Pokemon/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+
+import './styles.css';
 
 export default function Pokemon({ match }) {
     const { pokemons } = useSelector(({ pokemons }) => pokemons);
@@ -9,10 +11,33 @@ export default function Pokemon({ match }) {
 
     console.log(pokemon);
 
+    const handleEvolutionJoin = arr => {
+        return (
+            <Route>
+                {arr.map((pokemon, index)=>(
+                    <Link to={`/pokemon/${Number(pokemon.num)}`} key={index}>{pokemon.name}</Link>
+                ))}
+            </Route>
+        )
+    };
+
     return (
         <div>
-            <p>{pokemon.name}</p>
-            <Link to="/">Voltar</Link>
+            <h1>{pokemon.name}</h1>
+            <div className="detail-container">
+                <div className="detail-left">
+                    <img src={pokemon.img} alt={pokemon.name} />
+                </div>
+                <div className="detail-right">
+                    <ul>
+                        <li>Type: <span>{pokemon.type.join(" | ")}</span></li>
+                        <li>Weaknesses: <span>{pokemon.weaknesses.join(" | ")}</span></li>
+                        <li>Previous Evolution: <span>{pokemon.prev_evolution ? handleEvolutionJoin(pokemon.prev_evolution) : '---'}</span></li>
+                        <li>Next Evolution: <span>{pokemon.next_evolution ? handleEvolutionJoin(pokemon.next_evolution) : '---'}</span></li>
+                    </ul>
+                </div>
+                {/* <Link to="/">Voltar</Link> */}
+            </div>
         </div>
     )
 }
